a09342fbd88f20eaa3ee11c01228f167219bb4a8
shinopai <harbor.shinozaki@gmail.com>
2023-05-10 09:33:33 +0000

    ノウハウのダミーデータを生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
-99.times do
-  res = User.create!(
-    email: Faker::Internet.email,
-    password: 'password'
-  )
+# 99.times do
+#   res = User.create!(
+#     email: Faker::Internet.email,
+#     password: 'password'
+#   )
 
-  user_profile = res.build_user_profile
-  user_profile.save!
+#   user_profile = res.build_user_profile
+#   user_profile.save!
+# end
+
+350.times do
+  Knowhow.create!(
+    title: Faker::Address.country,
+    content: Faker::Lorem.sentence,
+    user_id: User.find(rand(1..User.count)).id,
+    category_id: Category.find(rand(1..Category.count)).id
+  )
 end
